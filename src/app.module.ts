c6224d2b3a27110b23aa7bf784cd77a37b1d706e
Adriano Anschau <adrianoanschau@gmail.com>
2020-07-25 14:52:36 -0300

    Configura módulo de GraphQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,20 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from "@nestjs/graphql";
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      definitions: {
+        path: join(process.cwd(), '/src/graphql.schema.d.ts'),
+        outputAs: 'class',
+      },
+      typePaths: ['./**/*.graphql'],
+      resolverValidationOptions: {
+        requireResolversForResolveType: false,
+      },
+    }),
+  ],
   controllers: [],
   providers: [],
 })
